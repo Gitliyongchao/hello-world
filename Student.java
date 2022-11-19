b6cd7eb4f07551514ebaedcb145bdda895a20f7c
2215640519@qq.com <2215640519@qq.com>
2022-11-20 00:30:41 +0800

    add some entitys

---
+++ Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    public String name;
+    public int firstAge;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFirstAge() {
+        return firstAge;
+    }
+
+    public void setFirstAge(int firstAge) {
+        this.firstAge = firstAge;
+    }
+}
